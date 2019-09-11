87d2a667b68a464c75ea27f967da4a4736eddced
Maxim Kovalevsky <maximkovalevsky@alexandrasyrbul.int.nordigy.ru>
2019-09-11 21:20:54 +0300

    bootstraping a simple form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Form, FormControl, Button, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Simple Notes App</h1>
+        <Row className="justify-content-md-center">
+          <Col md={6} offset={6}>
+            <Form>
+              <FormControl placeholder="Please type..." />
+              <br />
+              <Button>Submit</Button>
+            </Form>
+          </Col>
+        </Row>
       </header>
     </div>
   );
